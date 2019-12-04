f4e1909942b053afc5427741abbf5a03da8d3460
Chris Sewell <chrisj_sewell@hotmail.com>
2019-12-04 16:14:16 +0000

    add syntax highlighting of python (scoped by comments)

--- vscode-client/syntaxes/build_syntax.py
+++ vscode-client/syntaxes/build_syntax.py
@@ -5,7 +5,9 @@ import plistlib
 import yaml
 
 if __name__ == "__main__":
-    in_path = os.path.join(os.path.dirname(__file__), "restructuredtext.tmLanguage.yaml")
+    in_path = os.path.join(
+        os.path.dirname(__file__), "restructuredtext.tmLanguage.yaml"
+    )
     out_path = os.path.join(os.path.dirname(__file__), "restructuredtext.tmLanguage")
     with open(in_path) as fp:
         syntax = yaml.safe_load(fp)
