1daa19ceac1c4db24e7b5407b323100c9bf79ec3
Chris Sewell <chrisj_sewell@hotmail.com>
2019-12-05 17:39:33 +0000

    Create improved version of docutils Inliner and copy tests from docutils repo

---
+++ tests/test_docutils/inputs/create_inputs.py
@@ -0,0 +1,20 @@
+import yaml
+
+
+class MyDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super(MyDumper, self).increase_indent(flow, False)
+
+
+def dump_tests(totest, path):
+    data = {
+        k: [{"in": i.splitlines(), "out": o.splitlines()} for i, o in vs]
+        for k, vs in totest.items()
+    }
+    with open(path, "w") as handle:
+        yaml.dump(data, handle, Dumper=MyDumper, default_flow_style=False, indent=2)
+
+
+# totest = {}
+
+# dump_tests(totest, "/Users/cjs14/GitHub/rst-language-server/tests/docutils_test_data/test_inline_markup.yaml")
---
+++ tests/test_docutils/test_inliner.py
@@ -0,0 +1,69 @@
+import os
+
+from docutils import frontend, utils
+from docutils.parsers import rst
+import pytest
+import yaml
+
+from rst_lsp.docutils_ext.inliner_base import Inliner
+
+
+def load_yaml(path):
+    with open(path) as fp:
+        data = yaml.safe_load(fp)
+    return data
+
+
+def run_parser(case, inliner=None):
+    source = "\n".join(case["in"])
+    expected = "\n".join(case["out"])
+
+    parser = rst.Parser(inliner=inliner)
+    option_parser = frontend.OptionParser(components=(rst.Parser,))
+    settings = option_parser.get_default_values()
+    settings.report_level = 5
+    settings.halt_level = 5
+    # settings.debug = package_unittest.debug
+
+    document = utils.new_document("test data", settings)
+    parser.parse(source, document)
+    output = document.pformat()
+    # Normalize line endings:
+    if expected:
+        expected = "\n".join(expected.rstrip().splitlines())
+    if output:
+        output = "\n".join(output.rstrip().splitlines())
+
+    assert output == expected
+
+
+@pytest.mark.parametrize(
+    "name,number,case",
+    [
+        (name, i, case)
+        for name, cases in load_yaml(
+            os.path.join(
+                os.path.dirname(__file__), "inputs/test_inline_markup.yaml"
+            )
+        ).items()
+        for i, case in enumerate(cases)
+    ],
+)
+def test_inline_markup(name, number, case):
+    run_parser(case, inliner=Inliner())
+
+
+@pytest.mark.parametrize(
+    "name,number,case",
+    [
+        (name, i, case)
+        for name, cases in load_yaml(
+            os.path.join(
+                os.path.dirname(__file__), "inputs/test_interpreted.yaml"
+            )
+        ).items()
+        for i, case in enumerate(cases)
+    ],
+)
+def test_interpreted(name, number, case):
+    run_parser(case, inliner=Inliner())
